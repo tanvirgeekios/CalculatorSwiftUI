17140fc60130a3f8c1b9a145d4c65705107bd970
tanvirgeekios <tanvir.alam@iosventure.com>
2021-03-07 08:54:21 +0600

    Initial Commit

---
+++ March_Weekend_1_Day_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  March_Weekend_1_Day_2
+//
+//  Created by MD Tanvir Alam on 7/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ March_Weekend_1_Day_2/March_Weekend_1_Day_2App.swift
@@ -0,0 +1,17 @@
+//
+//  March_Weekend_1_Day_2App.swift
+//  March_Weekend_1_Day_2
+//
+//  Created by MD Tanvir Alam on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct March_Weekend_1_Day_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
